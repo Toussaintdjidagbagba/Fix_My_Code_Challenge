1588ae912c2cc88493158f8c6bf00b31c5db8cbc
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2023-03-16 18:28:59 +0100

    fix

--- 0-fizzbuzz.py
+++ 0-fizzbuzz.py
@@ -17,6 +17,7 @@ def fizzbuzz(n):
 
     tmp_result = ""
     for i in range(1, n + 1):
+        tmp_result += " "
         if (i%3 == 0 and i%5 == 0):
             tmp_result += "FizzBuzz"
         elif (i % 3) == 0:
